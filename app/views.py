828c24d740b88894da2c8ea5594a13157eccdb73
Ivan Semenenko <nlfa@yandex.ru>
2024-05-02 03:44:56 +0500

    first commit

---
+++ app/views.py
@@ -0,0 +1,200 @@
+﻿"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.http import HttpRequest
+from app.forms import AnketaForm
+from django.db import models
+from .models import Blog
+from .models import Comment
+from .forms import CommentForm
+from .forms import BlogForm
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Домашняя страница',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Контакты',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'О нас',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def anketa(request):
+    assert isinstance(request, HttpRequest)
+    data = None
+    gender = {'1': 'Мужчина', '2': 'Женщина'}
+    internet = {'1': 'Каждый день', '2': 'Несколько раз в день',
+                '3': 'Несколько раз в неделю', '4': 'Несколь раз в месяц'}
+    if request.method == 'POST':
+        form = AnketaForm(request.POST)
+        if form.is_valid():
+            data = dict()
+            data['name'] = form.cleaned_data['name']
+            data['city'] = form.cleaned_data['city']
+            data['job'] = form.cleaned_data['job']
+            data['gender'] = gender[ form.cleaned_data['gender'] ]
+            data['internet'] = internet[ form.cleaned_data['internet'] ]
+            if(form.cleaned_data['notice'] == True):
+                data['notice'] = 'Да'
+            else:
+                data['notice'] = 'Нет'
+            data['email'] = form.cleaned_data['email']
+            data['message'] = form.cleaned_data['message']
+            form = None
+    else:
+        form = AnketaForm()
+    return render(
+        request,
+        'app/anketa.html',
+        {
+            'form': form,
+            'data': data
+        }
+    )
+
+def registration(request):
+    if request.method == 'POST':
+        regform = UserCreationForm(request.POST)
+        if regform.is_valid():
+            reg_f = regform.save(commit=False)
+            reg_f.is_staff = False
+            reg_f.is_active = True
+            reg_f.is_cuperuser = False
+            reg_f.date_joined = datetime.now()
+            reg_f.last_login = datetime.now()
+            
+            regform.save()
+            return redirect('home')
+    else:
+         regform = UserCreationForm()
+    assert isinstance(request, HttpRequest)
+    return render (
+        request,
+        'app/registration.html',
+        {
+            'regform': regform,
+            'year':datetime.now().year,
+        }
+    )
+
+def blog(request):
+    posts = Blog.objects.all()
+    assert isinstance(request, HttpRequest)
+    return render (
+        request,
+        'app/blog.html',
+        {
+            'title': 'Блог',
+            'posts': posts,
+            'year': datetime.now().year,
+        }
+    )
+
+def blogpost(request, parameter):
+    post_1 = Blog.objects.get(id=parameter)
+    comments = Comment.objects.filter(post=parameter)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment_f = form.save(commit=False)
+            comment_f.author = request.user
+            comment_f.date = datetime.now()
+            comment_f.post = Blog.objects.get(id=parameter)
+            comment_f.save()
+            
+            return redirect('blogpost', parameter=post_1.id)
+    else:
+        form = CommentForm()
+        
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/blogpost.html',
+        {
+            'post_1': post_1,
+            'comments': comments,
+            'form': form,
+            'year': datetime.now().year
+        }
+    )
+
+def newpost(request):
+    assert isinstance(request, HttpRequest)
+    if request.method == "POST":
+        blogform = BlogForm(request.POST, request.FILES)
+        if blogform.is_valid():
+            blog_f = blogform.save(commit=False)
+            blog_f.posted = datetime.now()
+            blog_f.author = request.user
+            blog_f.save()
+            return redirect('blog')
+    else:
+        blogform = BlogForm()
+    
+    return render(
+        request,
+        'app/newpost.html',
+        {
+            'blogform': blogform,
+            'title': 'Добавить статью блога',
+            'year': datetime.now().year,
+        }
+    )
+
+def videopost(request):
+
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/videopost.html',
+        {
+            'title':'Видео',
+            'message':'ДА, это видео',
+            'year':datetime.now().year,
+        }
+    )
+
+def links(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/links.html',
+        {
+            'title':'Полезные ссылки',
+            'message':'Ссылочки',
+            'year':datetime.now().year
+        }
+    )
---
+++ weblabs/urls.py
@@ -0,0 +1,48 @@
+﻿"""
+Definition of urls for weblabs.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.conf.urls import include
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+import app
+
+admin.autodiscover()
+
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='app/login.html',
+             authentication_form=forms.BootstrapAuthenticationForm,
+             extra_context=
+             {
+                 'title': 'Log in',
+                 'year' : datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('admin/', admin.site.urls),
+    path('anketa/', app.views.anketa, name='anketa'),
+    path('registration/', app.views.registration, name='registration'),
+    path('blog/', app.views.blog, name='blog'),
+    path(r'P<parameter>/', app.views.blogpost, name='blogpost'),
+    path('newpost/', app.views.newpost, name='newpost'),
+    path('videopost/', app.views.videopost, name='videopost'),
+    path('links/', app.views.links, name='links')
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
